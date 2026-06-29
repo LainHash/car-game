d4301b8e77d435687c6584f319188213ba2889c4
Lain <chiyomiziwari@gmail.com>
2026-06-29 21:53:16 +0700

    Add drifting mechanics and animations to car gameplay

--- CarTest.gd
+++ CarTest.gd
@@ -6,6 +6,9 @@ extends Node3D
 @onready var RightWheel = $"Car/Model/wheel-front-right"
 @onready var LeftWheel = $"Car/Model/wheel-front-left"
 @onready var CarBody = $"Car/Model/body"
+@onready var DriftTimer = $DriftTimer
+@onready var BoostTimer = $BoostTimer
+@onready var Anim = $AnimationPlayer
 
 var acceleration = 70.0
 var steering = 12.0
@@ -16,20 +19,32 @@ var speed_input = 0
 var rotate_input = 0
 
 var Drifting = false
-var DriftingDirection = 0
+var DriftDirection = 0
 var MinimumDrift = false
 var Boost = 1
 var DriftBoost = 1.75
 
 func _physics_process(_delta):
 	Car.transform.origin = Ball.transform.origin
-	Ball.apply_central_force(-Car.global_transform.basis.z * speed_input)
+	Ball.apply_central_force(-Car.global_transform.basis.z * speed_input * Boost)
 	
 func _process(delta):
 	speed_input = (Input.get_action_strength("Accelerate") - Input.get_action_strength("Brake")) * acceleration
 	rotate_input = deg_to_rad(steering) * (Input.get_action_strength("SteerLeft") - Input.get_action_strength("SteerRight"))
 	RightWheel.rotation.y = rotate_input
 	LeftWheel.rotation.y = rotate_input
+	
+	if Input.is_action_just_pressed("Drift") and not Drifting and rotate_input != 0 and speed_input > 0:
+		StartDrift()
+	
+	if Drifting:
+		var DriftAmount = 0
+		DriftAmount += Input.get_action_strength("SteerLeft") - Input.get_action_strength("SteerRight")
+		DriftAmount *= deg_to_rad(steering * 0.55)
+		rotate_input = DriftDirection * DriftAmount
+		
+	if Drifting and (Input.is_action_just_pressed("Drift") or speed_input < 1):
+		StopDrift()
 	
 	if Ball.linear_velocity.length() > 0.75:
 		RotateCar(delta)
@@ -40,3 +55,29 @@ func RotateCar(delta):
 	Car.global_transform = Car.global_transform.orthonormalized()
 	var t = -rotate_input * Ball.linear_velocity.length() / body_tilt
 	CarBody.rotation.z = lerp(CarBody.rotation.z, t, 10 * delta)
+
+func StartDrift():
+	Drifting = true
+	Anim.play("Hop")
+	MinimumDrift = false
+	DriftDirection = rotate_input
+	DriftTimer.start()
+
+
+func StopDrift():
+	if MinimumDrift:
+		Boost = DriftBoost
+		BoostTimer.start()
+		Anim.play("ZoomOut")
+	Drifting = false
+	MinimumDrift = false
+
+
+func _on_drift_timer_timeout() -> void:
+	if Drifting:
+		MinimumDrift = true
+
+
+func _on_boost_timer_timeout() -> void:
+	Boost = 1.0
+	Anim.play("ZoomIn")
